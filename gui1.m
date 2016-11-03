fba9100852135e410cf917600d45fa595c7e2164
Vladislavs Ignatjevs <gadzi4ok@gmail.com>
2016-11-03 23:14:17 +0000

    added simple gui and single matlab line to convert img to matrix

---
+++ gui1.m
@@ -0,0 +1,172 @@
+function varargout = gui1(varargin)
+% GUI1 MATLAB code for gui1.fig
+%      GUI1, by itself, creates a new GUI1 or raises the existing
+%      singleton*.
+%
+%      H = GUI1 returns the handle to a new GUI1 or the handle to
+%      the existing singleton*.
+%
+%      GUI1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI1.M with the given input arguments.
+%
+%      GUI1('Property','Value',...) creates a new GUI1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before gui1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to gui1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help gui1
+
+% Last Modified by GUIDE v2.5 03-Nov-2016 23:11:18
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @gui1_OpeningFcn, ...
+                   'gui_OutputFcn',  @gui1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before gui1 is made visible.
+function gui1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to gui1 (see VARARGIN)
+
+% Choose default command line output for gui1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes gui1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = gui1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in generateButton.
+function generateButton_Callback(hObject, eventdata, handles)
+% hObject    handle to generateButton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function numPixels_Callback(hObject, eventdata, handles)
+% hObject    handle to numPixels (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of numPixels as text
+%        str2double(get(hObject,'String')) returns contents of numPixels as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function numPixels_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to numPixels (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function numTiles_Callback(hObject, eventdata, handles)
+% hObject    handle to numTiles (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of numTiles as text
+%        str2double(get(hObject,'String')) returns contents of numTiles as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function numTiles_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to numTiles (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function targetPath_Callback(hObject, eventdata, handles)
+% hObject    handle to targetPath (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of targetPath as text
+%        str2double(get(hObject,'String')) returns contents of targetPath as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function targetPath_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to targetPath (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function imgPath_Callback(hObject, eventdata, handles)
+% hObject    handle to imgPath (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of imgPath as text
+%        str2double(get(hObject,'String')) returns contents of imgPath as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function imgPath_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to imgPath (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
---
+++ image_converter.m
@@ -0,0 +1,6 @@
+%Computer vision assignment 
+%convert images to matrices in a loop
+
+image_test = imread('Images\out_manmade_1k\sun_aaasertfihkcjvdd.jpg');
+
+guide
