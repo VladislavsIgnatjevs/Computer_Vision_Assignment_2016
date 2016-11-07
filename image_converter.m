4783823ee850bdf56c08f6ec9f7ee11ece1ee868
VladislavsIgnatjevs <v.ignatjevs@dundee.ac.uk>
2016-11-07 21:47:03 +0000

    improved gui, added functionality for selecting paths folders files and target size, dimension, imput evaluation

--- image_converter.m
+++ image_converter.m
@@ -7,21 +7,25 @@ clear all
  source_imgs_dir_path = '';
  target_imgs_dir_path = '';
  
-global imgPath targetPath generateButton
+global numTiles numPixels generateButton output_folder_path InputFileName InputPathName InputFilterIndex input_folder_path
 gui1;
 
 
+
+
+     
+     
+
+ 
+     
+
+     
 if generateButton == 1
     source_imgs_dir_path = imgPath;
     target_imgs_dir_path = targetPath;
-end
-   % h = msgbox(source_imgs_dir_path, target_imgs_dir_path);
 
-%source_imgs_dir_path = imgPath;
-%source_imgs_dir_path = get(gui1.imgPath, 'String');
-%target_imgs_dir_path = targetPath;
-%target_imgs_dir_path = evalin('base', 'targetPath');
 
+%add to path enviroment if not empty
 if (~isempty(source_imgs_dir_path) ) && (~isempty(target_imgs_dir_path))
   
     
@@ -31,7 +35,12 @@ if (~isempty(source_imgs_dir_path) ) && (~isempty(target_imgs_dir_path))
     setenv('TARGET_IMGS_DIR', target_imgs_dir_path);
     getenv('SOURCE_IMGS_DIR');
     
-    h = msgbox(source_imgs_dir_path, target_imgs_dir_path);
+    
+ 
+
+    
+    
+
 end
 
 %image_test = imread('Images\out_manmade_1k\sun_aaasertfihkcjvdd.jpg');
