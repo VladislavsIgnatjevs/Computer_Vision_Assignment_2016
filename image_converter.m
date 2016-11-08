051e7deb05df7c070546add5840e20cd56a9d7d9
VladislavsIgnatjevs <v.ignatjevs@dundee.ac.uk>
2016-11-08 01:47:40 +0000

    added io selection functionality, started environment preparation function

--- image_converter.m
+++ image_converter.m
@@ -1,46 +1,20 @@
-%Computer vision assignment 
-%convert images to matrices in a loop
+% Computer vision assignment
+% convert images to matrices with gui
+
+% @author Yang Xu
+% @author Wei Li
+% @author Vladislavs Ignatjevs
 
 clear all
-%when generate button pressed get images source and write path
 
- source_imgs_dir_path = '';
- target_imgs_dir_path = '';
- 
-global numTiles numPixels generateButton output_folder_path InputFileName InputPathName InputFilterIndex input_folder_path
+
+global err numTiles numPixels generateButton output_folder_path InputFileName InputPathName InputFilterIndex input_folder_path
 gui1;
 
 
-
-
-     
-     
-
- 
-     
-
-     
-if generateButton == 1
-    source_imgs_dir_path = imgPath;
-    target_imgs_dir_path = targetPath;
-
-
-%add to path enviroment if not empty
-if (~isempty(source_imgs_dir_path) ) && (~isempty(target_imgs_dir_path))
-  
-    
-    setenv('SOURCE_IMGS_DIR', source_imgs_dir_path);
-    getenv('SOURCE_IMGS_DIR');
-
-    setenv('TARGET_IMGS_DIR', target_imgs_dir_path);
-    getenv('SOURCE_IMGS_DIR');
-    
-    
- 
-
-    
-    
-
-end
-
 %image_test = imread('Images\out_manmade_1k\sun_aaasertfihkcjvdd.jpg');
+
+
+
+
+
---
+++ prepareEnvironment.m
@@ -0,0 +1,30 @@
+% prepares environment and converts files to matrices
+function prepareEnvironment(imputPath, outputPath, fileNameArray, numPixels, numTiles)
+
+    
+    msgbox({imputPath,outputPath});
+    
+    % if generateButton == 1
+    %     source_imgs_dir_path = imgPath;
+    %     target_imgs_dir_path = targetPath;
+    %
+    % end
+    
+    %add to path enviroment if not empty
+%     if (~isempty(source_imgs_dir_path) ) && (~isempty(target_imgs_dir_path))
+%         
+%         
+%         setenv('SOURCE_IMGS_DIR', source_imgs_dir_path);
+%         getenv('SOURCE_IMGS_DIR');
+%         
+%         setenv('TARGET_IMGS_DIR', target_imgs_dir_path);
+%         getenv('SOURCE_IMGS_DIR');
+%         
+%         
+%         
+%         
+%         
+%         
+%         
+%     end
+end
