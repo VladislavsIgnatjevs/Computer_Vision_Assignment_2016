9b30c95856fca5995033c353919d9a17c0b28d9e
ShrugerXu <nc-xy@163.com>
2016-11-13 23:36:01 +0800

    Add files via upload
    matrix size fix

--- dHasher.m
+++ dHasher.m
@@ -1,7 +1,7 @@
 function out=dHasher(input)%input is original or scaled RGB image
 resized=imresize(input,[8 9]);
 gray=rgb2gray(resized);%grayscale
-fingerprint=zeros(8,9)
+fingerprint=zeros(8,8)
 for i=1:1:8
     for j=1:1:8
         if gray(i,j)>gray(i,j+1)
